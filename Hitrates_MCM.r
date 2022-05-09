4e4377246e0880266338492436efd9ccb851cfd6
CENTRAL\rubenvv <rubenvv@inditex.com>
2022-05-09 18:04:15 +0200

    Starting date added and sufix added to the dataset with the performance week

--- Hitrates_MCM.r
+++ Hitrates_MCM.r
@@ -22,7 +22,14 @@ substrLeft <- function(x, n){
 
 ###############################################################################
 ###############################################################################
+##    Constants
 ###############################################################################
+###############################################################################
+
+
+Starting_date<-as.Date("2022-03-21")
+
+
 ###############################################################################
 
 
@@ -35,3 +42,26 @@ inputdir<-paste(mydir,"/Input",sep="")
 
 #List files to upload from input dir
 dw_files<-list.files(path=inputdir)
+
+
+#upload n files and save them into Input_file_n  tibbles
+
+for(i in (1:length(dw_files))){
+  sufix<-paste(format(Starting_date+i*7,'%Y'),format(Starting_date+i*7,'%m'),format(Starting_date+i*7,'%d'),sep="")
+  
+  exp1 <- expression(paste("Input_file",sufix,sep="_"))                                              # Create name of the tibble expression
+  exp2<- expression(read_delim(file=paste(inputdir,dw_files[i],sep="/"),delim=";",col_names=TRUE))   # Create read_delim expression
+  z<-paste(eval(exp1),exp2,sep="<-")                                                                 # Create assignation expression as a string
+  eval(parse(text=z))                                                                                # Evaluate expression
+}
+
+#Get week on wich each Input_file_n was performed
+
+for(i in (1:length(dw_files)))
+{
+  sufix<-paste(format(Starting_date+i*7,'%Y'),format(Starting_date+i*7,'%m'),format(Starting_date+i*7,'%d'),sep="")
+  print(sufix)
+}
+
+
+
