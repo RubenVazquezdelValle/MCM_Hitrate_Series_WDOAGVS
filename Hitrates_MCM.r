d6148a121cd80e00e4425f2dcc9ccf028a13940e
CENTRAL\rubenvv <rubenvv@inditex.com>
2022-05-10 15:45:49 +0200

    Added week to each input dataset Input_file_yyyymmdd

--- Hitrates_MCM.r
+++ Hitrates_MCM.r
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+rm(list = ls())
 
 
 ###############################################################################
@@ -44,10 +44,10 @@ inputdir<-paste(mydir,"/Input",sep="")
 dw_files<-list.files(path=inputdir)
 
 
-#upload n files and save them into Input_file_n  tibbles
+#upload n files and save them into Input_file_yyymmdd  tibbles
 
 for(i in (1:length(dw_files))){
-  sufix<-paste(format(Starting_date+i*7,'%Y'),format(Starting_date+i*7,'%m'),format(Starting_date+i*7,'%d'),sep="")
+  sufix<-paste(format(Starting_date+(i-1)*7,'%Y'),format(Starting_date+(i-1)*7,'%m'),format(Starting_date+(i-1)*7,'%d'),sep="")
   
   exp1 <- expression(paste("Input_file",sufix,sep="_"))                                              # Create name of the tibble expression
   exp2<- expression(read_delim(file=paste(inputdir,dw_files[i],sep="/"),delim=";",col_names=TRUE))   # Create read_delim expression
@@ -55,13 +55,17 @@ for(i in (1:length(dw_files))){
   eval(parse(text=z))                                                                                # Evaluate expression
 }
 
-#Get week on wich each Input_file_n was performed
-
+#Get week on each Input_file_yyyymmdd through cbind expression cbind(Input_file_sufix,week=sufix)
+ 
 for(i in (1:length(dw_files)))
 {
-  sufix<-paste(format(Starting_date+i*7,'%Y'),format(Starting_date+i*7,'%m'),format(Starting_date+i*7,'%d'),sep="")
-  print(sufix)
+  sufix<-paste(format(Starting_date+(i-1)*7,'%Y'),format(Starting_date+(i-1)*7,'%m'),format(Starting_date+(i-1)*7,'%d'),sep="")
+  exp1 <- expression(paste("Input_file",sufix,sep="_"))                                              # Get name of the tibble expression
+  exp2<- paste("cbind(",eval(exp1),",week=sufix)",sep="")                                            # Create cbind expression
+  z<-paste(eval(exp1),eval(exp2),sep="<-")                                                           # Create assignation expression as a string
+  eval(parse(text=z))                                                                                # Evaluate expression
 }
 
 
-
+##Results<-rbind(Results,tibble(method = "Movie Bias", RMSE = movbias_rmse, MAE = movbias_mae))
+##cbind(Input_file_20220321,week=sufix)
